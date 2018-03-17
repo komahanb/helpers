f5cd01bfb282cca7c03474f20b4e7f40f8a40989
Komahan Boopathy <komahan.cool@gmail.com>
2018-03-17 13:55:58 -0400

    added packages

---
+++ packages.sh
@@ -0,0 +1,12 @@
+sudo apt install kazam
+sudo apt install blender
+sudo apt install xxdiff
+sudo apt install meld
+sudo apt install libfreetype6-dev
+sudo apt install freecad
+
+# Third party
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt install spotify-client
