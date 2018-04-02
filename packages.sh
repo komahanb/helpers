2b4cd34d766912fe27c7e496fa895520bf3ec520
Komahan Boopathy <komahan.cool@gmail.com>
2018-04-02 12:02:41 -0400

    added mendeley to third party

--- packages.sh
+++ packages.sh
@@ -10,3 +10,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt install spotify-client
+
+# Mendeley
+wget https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest
+sudo dpkg -i mendeleydesktop-latest
